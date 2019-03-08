807662f44f095262330106d004a0cad4d8c31221
sandeepyadav02 <etawah2010@gmail.com>
2019-03-08 13:19:02 +0530

    final

--- src/main/resources/.sql
+++ src/main/resources/.sql
@@ -1,9 +1,29 @@
 -- Create syntax for TABLE 'patient'
 CREATE TABLE `patient` (
-  `id` varchar(32) NOT NULL,
+  `id` int(32) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `gender` varchar(45) DEFAULT NULL,
   `surname` varchar(45) DEFAULT NULL,
   `date` date DEFAULT NULL,
+  `updateDateTime` date DEFAULT NULL,
+  `createDateTime` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create syntax for TABLE 'patient_add'
+CREATE TABLE `patient_add` (
+  `id` int(32) NOT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `patient_id` int(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create syntax for TABLE 'patient_contact'
+CREATE TABLE `patient_contact` (
+  `id` int(32) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `patient_id` int(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
